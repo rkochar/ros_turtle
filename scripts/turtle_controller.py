e6451825c0065e516cecb2662fce7e6f66834647
Daan Goossens <daangoossens22@gmail.com>
2020-10-12 12:21:45 +0200

    fixed publisher error

--- scripts/turtle_controller.py
+++ scripts/turtle_controller.py
@@ -4,7 +4,7 @@ from std_msgs.msg import String
 from ros_turtle.msg import Move 
 from geometry_msgs.msg import Twist
 
-pub = None
+pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
 def callback(data):
     # pub = rospy.Publisher('/turtleass/turtle1/cmd_vel', Twist, queue_size=10)
@@ -15,6 +15,11 @@ def callback(data):
         rospy.loginfo("square")
         twist_message = Twist()
         twist_message.linear.x = 2
+        twist_message.linear.y = 0
+        twist_message.linear.z = 0
+        twist_message.angular.x = 0
+        twist_message.angular.y = 0
+        twist_message.angular.z = 0
         pub.publish(twist_message)
     elif data.type == "reset":
         rospy.loginfo("reset")
@@ -25,8 +30,8 @@ def callback(data):
 def listener():
     rospy.init_node('turtle_controller', anonymous=True)
 
-    pub = rospy.Publisher('/turtleass/turtle1/cmd_vel', Twist, queue_size=10)
     rospy.Subscriber('move_turtle', Move, callback)
+#    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
     rospy.spin()
 
