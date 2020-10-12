c8b97b4425aab6cf046761b3eca5cacf4519c3dc
Daan Goossens <daangoossens22@gmail.com>
2020-10-12 12:44:13 +0200

    make square functionality added

--- scripts/turtle_controller.py
+++ scripts/turtle_controller.py
@@ -3,8 +3,31 @@ import rospy
 from std_msgs.msg import String
 from ros_turtle.msg import Move 
 from geometry_msgs.msg import Twist
+import math
 
 pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+
+def move_forward():
+    twist_message = Twist()
+    twist_message.linear.x = 1
+    pub.publish(twist_message)
+
+def turn_left():
+    twist_message = Twist()
+    twist_message.angular.z = -math.pi/2
+    pub.publish(twist_message)
+
+def turn_and_move(): 
+    move_forward()
+    rospy.sleep(1)
+    turn_left()
+    rospy.sleep(1)
+
+def make_square():
+    turn_and_move()
+    turn_and_move()
+    turn_and_move()
+    turn_and_move()
 
 def callback(data):
     # pub = rospy.Publisher('/turtleass/turtle1/cmd_vel', Twist, queue_size=10)
@@ -13,14 +36,7 @@ def callback(data):
         rospy.loginfo("cirlce")
     elif data.type == "square":
         rospy.loginfo("square")
-        twist_message = Twist()
-        twist_message.linear.x = 2
-        twist_message.linear.y = 0
-        twist_message.linear.z = 0
-        twist_message.angular.x = 0
-        twist_message.angular.y = 0
-        twist_message.angular.z = 0
-        pub.publish(twist_message)
+        make_square()
     elif data.type == "reset":
         rospy.loginfo("reset")
     else:
