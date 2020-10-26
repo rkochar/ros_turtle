ff69e7a25c006fac4ac5aeccd7f283dcee8dbd0b
rkochar <rkochar9@gmail.com>
2020-10-26 18:38:38 +0530

    Added comments

--- scripts/turtle_controller.py
+++ scripts/turtle_controller.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
 from ros_turtle.msg import Move
 from geometry_msgs.msg import Twist
 import math
@@ -8,35 +7,62 @@ from std_srvs.srv import Empty
 from turtlesim.srv import TeleportAbsolute
 from ros_turtle.srv import SetSpeed, SetSpeedResponse
 
+# Make publisher to turtle and global variable speed with value 3.
 pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-global speed
 speed = 3
 
-def move_forward(leng, speed):
+
+def move_forward(velocity):
+    """
+    Moves turtle forward by specified distance.
+
+    :param velocity: X component of linear velocity of turtle
+    :return: None
+    """
     twist_message = Twist()
-    twist_message.linear.x = speed
+    twist_message.linear.x = velocity
     pub.publish(twist_message)
 
-def turn_left(rad, speed):
+
+def turn_left(angular_velocity):
+    """
+    Turns the turtle direction by specified radians.
+    :param angular_velocity: Z component of angular velocity to turn turtle (in radians).
+    :return: None
+    """
     twist_message = Twist()
     twist_message.linear.x = 0
-    twist_message.angular.z = speed
+    twist_message.angular.z = angular_velocity
     pub.publish(twist_message)
 
-def make_square(leng):
+
+def make_square(distance):
+    """
+    Makes a square with the turtle of specified side.
+
+    :param distance: Side of square.
+    :return: None
+    """
     global speed
-    time_to_wait = leng / speed
-    move_forward(leng, speed)
+    time_to_wait = distance / speed
+    move_forward(distance, speed)
     rospy.sleep(time_to_wait)
     pub.publish(Twist())
 
-    rad = math.pi/2
+    rad = math.pi / 2
     time_to_wait_turn = rad / speed
-    turn_left(rad, speed)
+    turn_left(speed)
     rospy.sleep(time_to_wait_turn)
     pub.publish(Twist())
 
+
 def make_circle(radius):
+    """
+    Makes a circle of specified radius with the turtle.
+
+    :param radius: Radius of circle.
+    :return: None
+    """
     global speed
     linear_speed = speed
     circumference = 2 * math.pi * radius
@@ -46,10 +72,12 @@ def make_circle(radius):
     twist_message = Twist()
     twist_message.linear.x = linear_speed
     twist_message.angular.z = angular_speed
+
+    # 0.9 is less than 1s timeout of turtle.
     while True:
-        if (time_taken > 0.9):
+        if time_taken > 0.9:
             pub.publish(twist_message)
-            rospy.sleep (0.9)
+            rospy.sleep(0.9)
             time_taken -= 0.9
         else:
             pub.publish(twist_message)
@@ -57,21 +85,20 @@ def make_circle(radius):
             pub.publish(Twist())
             break
 
-#    global speed
-#    twist_message = Twist()
-#    s = radius * 2 * math.pi #* speed
-
-#    twist_message.linear.x = s
-#    twist_message.angular.z = s / radius
-#    pub.publish(twist_message)
 
 def callback(data):
-    rate = rospy.Rate(10)
+    """
+    Callback method of topic "Move".
 
+    :param data: Object of msg Move.
+    :return: None
+    """
+    # Circle
     if data.type == "circle":
         rospy.loginfo("circle")
         make_circle(data.range)
 
+    # Square
     elif data.type == "square":
         rospy.loginfo("Square side: '{0}'".format(
             str(data.range)))
@@ -80,46 +107,59 @@ def callback(data):
         for i in range(4):
             make_square(data.range)
 
+    # Reset turtle to centre and clean the screen.
     elif data.type == "reset":
         rospy.loginfo("reset")
         rospy.ServiceProxy('reset', Empty)()
 
-    elif data.type == "centre":
+    # Centre the turtle.
+    elif data.type == "center":
         rospy.loginfo("Centre")
-        rospy.ServiceProxy('turtle1/teleport_absolute',
-                TeleportAbsolute)(5.5, 5.5, 0)
+        rospy.ServiceProxy('turtle1/teleport_absolute', TeleportAbsolute)(5.5, 5.5, 0)
 
-    elif data.type == "move":
-        rospy.loginfo("Move")
-        make_square(data.range)
+    # # Change speed.
+    # elif data.type == 'speed':
+    #     global speed
+    #     speed *= data.range
+    #     rospy.loginfo("Speed '{0}' ".format(float(speed)))
 
-    elif data.type == 'speed':
-        global speed
-        speed *= data.range
-        rospy.loginfo("Speed '{0}' ".format(float(speed)))
-
+    # Default case.
     else:
         rospy.loginfo("Invalid type: ", data.type)
 
+
 def speed_callback(data):
-#    rate = rospy.Rate(10)
-#    if type(data) == float or type(data) == int:
-#        #callback(data_object)
-#        rospy.loginfo("Speed service: ", data)
-        global speed
-        speed *= data.speed
-        return SetSpeedResponse()
+    """
+    Callback of rosservice speed.
+
+    :param data: Object of srv SetSpeed.
+    :return: SetSpeedResponse
+    """
+    global speed
+    speed *= data.speed
+    return SetSpeedResponse()
+
 
 def listener():
+    """
+    Listens to the topic "move_turtle" and service "set_speed" and calls the appropriate callback methods.
+    :return: None
+    """
     rospy.init_node('turtle_controller', anonymous=True)
 
+    # Listen to topic and service. Throw exception when there is an unexpected error.
     try:
         rospy.Subscriber('move_turtle', Move, callback)
         rospy.Service('set_speed', SetSpeed, speed_callback)
     except rospy.ServiceException as exp:
         print("ServiceException in turtle_controller.py", + str(exp))
 
+    # Spins forever.
     rospy.spin()
 
+
 if __name__ == '__main__':
+    """
+    Main method.
+    """
     listener()
